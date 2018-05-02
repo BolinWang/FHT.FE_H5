faa3e7393eff759ee1013992bca27330aed39ccc
褚德权 <chudequan@chudequandeMacBook-Pro.local>
2018-05-02 14:23:23 +0800

    添加麦邻搜索找房列表页代码

---
+++ search-room/search-list/webpack.config.js
@@ -0,0 +1,80 @@
+const resolve = require('path').resolve
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const url = require('url')
+const publicPath = ''
+
+module.exports = (options = {}) => ({
+  entry: {
+    vendor: './src/vendor',
+    index: './src/main.js'
+  },
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: options.dev ? '[name].js' : 'js/[name].js?[chunkhash]',
+    chunkFilename: '[id].js?[chunkhash]',
+    publicPath: options.dev ? '/assets/' : publicPath
+  },
+  module: {
+    rules: [{
+        test: /\.vue$/,
+        use: ['vue-loader']
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: 'images/[hash:8].[ext]'
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      title: '麦邻生活',
+      template: resolve(__dirname, 'src/index.html'),
+      favicon: resolve(__dirname, 'src/favicon.ico')
+    })
+  ],
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'src')
+    }
+  },
+  devServer: {
+    host: '192.168.5.105',
+    port: 8010,
+    proxy: {
+      '/api': {
+        target: 'https://api.mdguanjia.com/myhome/api/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+    historyApiFallback: {
+      index: url.parse(options.dev ? '/assets/' : publicPath).pathname
+    }
+  },
+  devtool: options.dev ? '#eval-source-map' : ''
+})
