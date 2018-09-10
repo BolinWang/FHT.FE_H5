8e4e85df0c39f36f17bd01293ba2009cd57b0f9d
weichen12 <2227538634@qq.com>
2018-09-10 18:39:49 +0800

    修改

--- FHD/src/views/perlperformance.vue
+++ FHD/src/views/perlperformance.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-     <iframe width="100%" :src="nowUrl"></iframe>
+     <iframe width="100%"  height='400' style='min-height: 400px' :src="nowUrl"></iframe>
   </div>
 </template>
 <script>
@@ -35,7 +35,7 @@ export default {
     window['refreshPage'] = (row) => {
       const jsonList = JSON.parse(row)
       that.upDataUrl(jsonList)
-      return '124'
+
     }
     axios.get(`${biUrl}?op=fs_load&cmd=sso`, {
       params: {
@@ -50,21 +50,24 @@ export default {
   },
   methods: {
     upDataUrl(jsonList) {
+      console.log(this.$route.query.type)
       if (jsonList) {
+        this.startDate = jsonList.startTime
+        this.endDate = jsonList.endTime
         switch (this.$route.query.type) {
-          case 1:
-            this.startDate = this.$route.query.startDate
-            this.endDate = this.$route.query.endDate
-            this.nowUrl = `http://bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B4e2a%5D%5B4eba%5D%5B4e1a%5D%5B7ee9%5D.cpt&op=h5&fid=${this.userData.fid}&startDate=${this.startDate}&endDate=${this.endDate}`
+          case '1':
+            this.nowUrl = `http://bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B4e2a%5D%5B4eba%5D%5B4e1a%5D%5B7ee9%5D.cpt&op=h5&fid=${this.userData.id}&startDate=${this.startDate}&endDate=${this.endDate}`
             break
-          case 2:
+          case '2':
             this.userData.leafDep === true
-            ? this.nowUrl = `http://bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D.cpt&op=h5&depId=${this.userData.depId}&&startDate=${this.startDate}&endDate=${this.endDate}`
-            : this.nowUrl = `http://bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D-%5B975e%5D%5B6700%5D%5B540e%5D%5B4e00%5D%5B7ea7%5D.cpt&op=h5&depId=${this.userData.depId}&&startDate=${this.startDate}&endDate=${this.endDate}`
+            ? this.nowUrl = `http://bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D.cpt&op=h5&depId=${jsonList.depName}&&startDate=${this.startDate}&endDate=${this.endDate}`
+            : this.nowUrl = `http://bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D-%5B975e%5D%5B6700%5D%5B540e%5D%5B4e00%5D%5B7ea7%5D.cpt&op=h5&depId=${jsonList.depName}&&startDate=${this.startDate}&endDate=${this.endDate}`
+            break
+            default:
             break
         }
       } else {
-        this.nowUrl = `http://bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B4e2a%5D%5B4eba%5D%5B4e1a%5D%5B7ee9%5D.cpt&op=h5&fid=${this.userData.fid}&startDate=${this.startDate}&endDate=${this.endDate}`
+        this.nowUrl = `http://bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B4e2a%5D%5B4eba%5D%5B4e1a%5D%5B7ee9%5D.cpt&op=h5&fid=${this.userData.id}&startDate=${this.startDate}&endDate=${this.endDate}`
       }
     }
   }
@@ -75,4 +78,5 @@ export default {
     min-height: 100%;
     background: #fff;
   }
+
 </style>
