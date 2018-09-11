019157b8dcf436748a367092c82a9d95ee034a23
weichen12 <2227538634@qq.com>
2018-09-11 20:13:27 +0800

    修改

--- FHD/src/views/perlperformance.vue
+++ FHD/src/views/perlperformance.vue
@@ -70,8 +70,8 @@ export default {
           case '2':
             console.log(jsonList.leafDep)
             jsonList.leafDep === true
-            ? this.nowUrl = `//bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D.cpt&op=h5&depId=${jsonList.depId}&&startDate=${this.startDate}&endDate=${this.endDate}`
-            : this.nowUrl = `//bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D-%5B975e%5D%5B6700%5D%5B540e%5D%5B4e00%5D%5B7ea7%5D.cpt&op=h5&depId=${jsonList.depId}&&startDate=${this.startDate}&endDate=${this.endDate}`
+            ? this.nowUrl = `//bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D.cpt&op=h5&depId=${jsonList.depId}&&startDate=${this.startDate}&endDate=${this.endDate}&__bypagesize__=false`
+            : this.nowUrl = `//bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D-%5B975e%5D%5B6700%5D%5B540e%5D%5B4e00%5D%5B7ea7%5D.cpt&op=h5&depId=${jsonList.depId}&&startDate=${this.startDate}&endDate=${this.endDate}&__bypagesize__=false`
             break
           default:
             break
@@ -81,8 +81,8 @@ export default {
           this.nowUrl = `//bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B4e2a%5D%5B4eba%5D%5B4e1a%5D%5B7ee9%5D.cpt&op=h5&fid=${this.userData.id}&startDate=${this.startDate}&endDate=${this.endDate}`
         } else {
           this.userData.leafDep === true
-            ? this.nowUrl = `//bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D.cpt&op=h5&depId=${this.userData.depId}&&startDate=${this.startDate}&endDate=${this.endDate}`
-            : this.nowUrl = `//bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D-%5B975e%5D%5B6700%5D%5B540e%5D%5B4e00%5D%5B7ea7%5D.cpt&op=h5&depId=${this.userData.depId}&&startDate=${this.startDate}&endDate=${this.endDate}`
+            ? this.nowUrl = `//bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D.cpt&op=h5&depId=${this.userData.depId}&&startDate=${this.startDate}&endDate=${this.endDate}&__bypagesize__=false`
+            : this.nowUrl = `//bi.mdguanjia.com/fh/ReportServer?reportlet=fh%2FcityManagerAPP%2F%5B7ba1%5D%5B7406%5D%5B65e5%5D%5B62a5%5D-%5B975e%5D%5B6700%5D%5B540e%5D%5B4e00%5D%5B7ea7%5D.cpt&op=h5&depId=${this.userData.depId}&&startDate=${this.startDate}&endDate=${this.endDate}&__bypagesize__=false`
         }
       }
     }
