e59c6cf53db1f50a2fb01abf889d8cc42453d847
王玲波 <454949876@qq.com>
2018-10-11 16:15:06 +0800

    ### *柏林* 麦邻协议App Store

---
+++ mailinProto/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <section class="agreeTxt" v-html="userAgreeMent"></section>
+  </div>
+</template>
+
+<script>
+import userAgreeMent from '@/utils/mlAgree'
+export default {
+  name: 'app',
+  data () {
+    return {
+      userAgreeMent
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './styles/index.scss';
+.agreeTxt {
+  width: 750px;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
