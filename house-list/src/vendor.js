1bd0ce1886fb8fe7fba36ed877758bb1f4cc87fe
褚德权 <chudequan_48@163.com>
2018-10-19 15:42:14 +0800

    静态资源打包优化，css单独打包

--- house-list/src/vendor.js
+++ house-list/src/vendor.js
@@ -2,3 +2,15 @@ import Vue from 'vue'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import axios from 'axios'
+
+Vue.use(MintUI)
+
+import { Lazyload } from 'mint-ui'
+
+Vue.use(Lazyload, {
+  error: require('./assets/images/room-default.jpg'),
+  loading: require('./assets/images/room-default.jpg'),
+  attempt: 1
+})
+
+import './assets/css/main.css'
