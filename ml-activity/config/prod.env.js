f30ad36d63189c48d724ea3daf8284503b06a8a9
褚德权 <chudequan_48@163.com>
2018-11-06 18:19:57 +0800

    ##德权## 线上环境访问接口地址修改

--- ml-activity/config/prod.env.js
+++ ml-activity/config/prod.env.js
@@ -1,8 +1,8 @@
 /*
  * @Author: FT.FE.Bolin
  * @Date: 2018-08-17 16:08:45
- * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-10-18 14:00:23
+ * @Last Modified by: chudequan
+ * @Last Modified time: 2018-11-06 18:19:04
  */
 
 'use strict'
@@ -10,5 +10,6 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   BASE_API: '"//www.mdguanjia.com/activity/"',
-  ML_API: '"//api.mdguanjia.com/myhome/api/"'
+  ML_API: '"https://test.mdguanjia.com/myhome/"',
+  APP_DOWNLOAD_URL: '"https://tstatic190.mdguanjia.com/appGuides/index.html"'
 }
