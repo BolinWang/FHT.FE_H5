7fbf606db56fd21eda30971875e134f1a3a21ed0
褚德权 <chudequan_48@163.com>
2018-11-06 12:58:21 +0800

    测试环境配置文件修改

--- ml-activity/config/test.env.js
+++ ml-activity/config/test.env.js
@@ -2,7 +2,7 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-08-17 16:09:03
  * @Last Modified by: chudequan
- * @Last Modified time: 2018-11-06 12:53:23
+ * @Last Modified time: 2018-11-06 12:57:43
  */
 
 'use strict'
@@ -13,7 +13,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"test"',
   BASE_API: '"//test.mdguanjia.com/activity/"',
-  ML_API: '"https://test1.mdguanjia.com/myhome/"',
-  APP_DOWNLOAD_URL: '"https://tstatic201.mdguanjia.com/appGuides/index.html"'
+  ML_API: '"https://test.mdguanjia.com/myhome/"',
+  APP_DOWNLOAD_URL: '"https://tstatic190.mdguanjia.com/appGuides/index.html"'
 })
 
