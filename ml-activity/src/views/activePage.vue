09b56322c77059e3657a311d2fd7564d705da12f
褚德权 <chudequan_48@163.com>
2018-11-06 11:40:00 +0800

    测试代码 15

--- ml-activity/src/views/activePage.vue
+++ ml-activity/src/views/activePage.vue
@@ -353,7 +353,7 @@ export default {
             // 领取成功
             Dialog.alert({
               confirmButtonText: '立即查看使用',
-              message: `恭喜获得1111元租金券！`
+              message: '恭喜获得1111元租金券！'
             }).then(() => {
               this.toUseCoupon()
             })
